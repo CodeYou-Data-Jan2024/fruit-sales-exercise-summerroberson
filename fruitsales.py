6f16b416813b75f3960fbc343a57afd54588151b
summer-roberson <summerroberson05@gmail.com>
2024-09-22 20:47:24 -0400

    fixing errors

--- fruitsales.py
+++ fruitsales.py
@@ -1,14 +1,10 @@
 import pandas as pd
 
-df_fruit = pd.DataFrame (columns = ["Apples" , "Bananas" ])
-
-df_fruit
-
-new_data = {
+data = {
     "Apples" : [35 , 41],
     "Bananas" : [41 , 34]
 }
 
-df_sales = pd.DataFrame(new_data, index=["2017 Sales","2018 Sales"])
+df_sales = pd.DataFrame(data, index=["2017 Sales","2018 Sales"])
 
-df_sales
+print (df_sales)
