5be87883a482d10f8625397036350e057e114059
summer-roberson <summerroberson05@gmail.com>
2024-09-22 20:37:17 -0400

    added code to fruitsales.py

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,14 @@
-# add your code here
+import pandas as pd
+
+df_fruit = pd.DataFrame (columns = ["Apples" , "Bananas" ])
+
+df_fruit
+
+new_data = {
+    "Apples" : [35 , 41],
+    "Bananas" : [41 , 34]
+}
+
+df_sales = pd.DataFrame(new_data, index=["2017 Sales","2018 Sales"])
+
+df_sales
